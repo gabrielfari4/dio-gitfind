f305d264f79b6e5797f401a512b5095334db1ee0
Gabriel Faria <gabrielfaria88@gmail.com>
2024-05-21 17:45:36 -0300

    creating input/button components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,20 @@
 import Header from "../../components/Header";
-
+import background from '../../assets/background2.png'
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="Imagem github" />
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@user" />
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
